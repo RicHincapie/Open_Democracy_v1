adf7934b82564d7bf47e683b59c938007f209caf
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-10-12 15:47:35 +0000

    Add models/candidato.py adjusted to MySQL database data types and relationships

--- models/candidato.py
+++ models/candidato.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ holds class City"""
 import models
+from sqlalchemy.sql.sqltypes import SmallInteger
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, 
 from sqlalchemy.orm import relationship
 
 
@@ -12,13 +13,12 @@ class Candidato(BaseModel, Base):
     """Representation of candidato """
     if models.storage_t == "db":
         __tablename__ = 'candidatos'
-        partido_id = Column(String(60), ForeignKey('partidos.id'), nullable=False)
+        partido_id = Column(SmallInteger(1), ForeignKey('partidos.id'), nullable=False)
         nombre = Column(String(128), nullable=False)
         apellido = Column(String(128), nullable=False)
-        resultados_id = Column(String(128), nullable=False)
-        # Proposal to assure One to Many relationship
-        # resultados_id = relationship('Resultado')
-
+        resultados_id = relationship('Resultado')
+        # Proposal to assure One to Many relationship (changed!)
+        # resultados_id = Column(String(128), nullable=False) (JCs code)
     else:
         partido_id = ""
         nombre = ""
