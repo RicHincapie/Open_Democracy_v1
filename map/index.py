fc47de417cf202581071b4e081447c7a219907a5
Camilo Moncada <monk-da@hotmail.com>
2020-10-28 18:51:12 -0500

    corrections files index.html,about.html,landing.html,index.py and app.js

--- map/index.py
+++ map/index.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def hbnb():
     return render_template('index.html')
 
+@app.route('/', strict_slashes=False)
+def index():
+    return render_template('landing.html')
+
+@app.route('/about', strict_slashes=False)
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5002, debug=True)
